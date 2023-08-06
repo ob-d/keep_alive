42c3623a8d6948ea14aca73f68f26b428fbea8e5
susilogic <susitronix@gmx.net>
2023-08-06 20:28:16 +0200

    added comment

--- keep_alive.bat
+++ keep_alive.bat
@@ -1,8 +1,8 @@
 @echo off
 
 :loop
+  REM you can use any domain.
   ping -n 1 google.com
-  REM This is a debug line.
   echo "-----------------------------"
   echo "Packets returned with %ERRORLEVEL% Error"
   echo "-----------------------------"
