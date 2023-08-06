4727c773cf810b5d4eaebe8d775ac923f3c87422
susilogic <susitronix@gmx.net>
2023-08-06 20:25:46 +0200

    added cmd log

--- keep_alive.bat
+++ keep_alive.bat
@@ -2,5 +2,9 @@
 
 :loop
   ping -n 1 google.com
+  REM This is a debug line.
+  echo "-----------------------------"
+  echo "Packets returned with %ERRORLEVEL% Error"
+  echo "-----------------------------"
   timeout /t 90
   goto loop
