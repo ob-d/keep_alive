6a1be905eed95521696ac86368f1f32b02e311ac
susilogic <susitronix@gmx.net>
2023-08-10 17:08:42 +0200

    added comment

--- keep_alive.bat
+++ keep_alive.bat
@@ -1,11 +1,13 @@
 @echo off
 
 :loop
-  REM you can use any domain.
+  REM You can use any domain.
   ping -n 1 ob-d.com
   echo "-----------------------------"
   echo "Packets returned with %ERRORLEVEL% Error"
   echo "-----------------------------"
+  REM Enter the delay time in seconds.
   timeout /t 3
+  REM For keeping the log, simply comment the "cls" command.
   cls
   goto loop
