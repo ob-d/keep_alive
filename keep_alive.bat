164fd9f4e6d3de6e1c67b3899ed9f5e1f0b5ab2f
susilogic <susitronix@gmx.net>
2023-08-10 16:55:56 +0200

    edited domain

--- keep_alive.bat
+++ keep_alive.bat
@@ -2,7 +2,7 @@
 
 :loop
   REM you can use any domain.
-  ping -n 1 google.com
+  ping -n 1 idt0.com
   echo "-----------------------------"
   echo "Packets returned with %ERRORLEVEL% Error"
   echo "-----------------------------"
